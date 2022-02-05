d675c6edd7f96de1407b8c111e4c368fcf1e3ad8
Kevin Hankes <48036731+kevinmhankes@users.noreply.github.com>
2022-02-05 16:25:50 -0600

    Disable user input before the computer's turn finishes

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                                GridItem(.flexible())]
     
     @State private var moves: [Move?] = Array(repeating: nil, count: 9)
+    @State private var isGameboardDisabled = false
     
     var body: some View {
         GeometryReader { geometry in
@@ -35,20 +36,23 @@ struct ContentView: View {
                         .onTapGesture {
                             if isSquareOccupied(in: moves, forIndex: i) { return }
                             moves[i] = Move(player: .human, boardIndex: i)
+                            isGameboardDisabled = true
                             
                             // check for win condiiton or draw
                             
                             DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
                                 let computerPosition = determineComputerMovePosition(in: moves)
                                 moves[computerPosition] = Move(player: .computer, boardIndex: computerPosition)
+                                isGameboardDisabled = false
                             }
                         }
                     }
                 }
                 Spacer()
-            }.padding()
+            }
+            .disabled(isGameboardDisabled)
+            .padding()
         }
-        
     }
     
     func isSquareOccupied(in moves: [Move?], forIndex index: Int) -> Bool {
