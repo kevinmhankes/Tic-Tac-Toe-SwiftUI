44933eb2723ee401999b5b5aa96e2e07a18639ac
Kevin Hankes <48036731+kevinmhankes@users.noreply.github.com>
2022-02-05 15:37:34 -0600

    Initial commit

---
+++ Shared/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Shared
+//
+//  Created by Kevin Hankes on 2/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
